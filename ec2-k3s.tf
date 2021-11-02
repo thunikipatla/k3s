19ecec4855eef466d4c4af7c983adb20d73bedab
srinivas <48064666+thunikipatla@users.noreply.github.com>
2021-11-02 08:17:58 +0530

    Added ingress for the port from 0 to 65535

--- ec2-k3s.tf
+++ ec2-k3s.tf
@@ -21,6 +21,12 @@ resource "aws_security_group" "allow-ssh" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
   egress {
     from_port   = 22
